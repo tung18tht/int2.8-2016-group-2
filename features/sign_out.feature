714048eeb84014b47d7e5eace43111e804f7ca48
duy-tran <grimshady.z@gmail.com>
2016-04-10 20:04:19 +0700

    Modified sign out test

--- features/sign_out.feature
+++ features/sign_out.feature
@@ -4,8 +4,8 @@ I want to log out
 So that other users can log in
 
 Scenario: An user signing out
-Given I am on the home page as an logged in user
-And I click on sign out
-Then I should be redirected to the home page
+Given a logged in user
+When I click on sign out
+Then I should be redirected to home page
 And I should see a Sign out success message
 And I should see the Sign in button
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -55,24 +55,8 @@ Then(/^I should see the Sign out option$/) do
 end
 
 #Signing out test
-Given(/^I am on the home page as an logged in user$/) do
-   @user = User.create!({
-             :email => "testing@usth.edu.vn",
-             :password => "12345678",
-             :password_confirmation => "12345678"
-           })
-  visit new_user_session_path
-  fill_in "Email", :with => 'testing@usth.edu.vn'
-  fill_in "Password", :with => '12345678'
-  click_button "Log in"
-  visit root_path
-  expect(page).to have_content("New Post")
-end
 When(/^I click on sign out$/) do
   click_on "Sign Out"
-end
-Then(/^I should be redirected to the home page$/) do
-  expect(current_path).to eq(root_path)
 end
 Then(/^I should see a Sign out success message$/) do
   expect(page).to have_content("Signed out successfully.")
