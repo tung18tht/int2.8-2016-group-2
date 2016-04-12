240ce1a4219de187c00408a94e5fbd05e0bb9779
rosemarytran <ththao111@gmail.com>
2016-04-12 03:40:00 +0000

    Add basic current user prfile. Add 'Find friends' option

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   #                                                 unlock: 'unblock', registration: 'register', 
   #                                                   sign_up: 'sign_up' }
   resources :posts
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
