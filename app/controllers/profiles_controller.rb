1c828ed9381304de2ae1702082fba17c7b6ac897
duy-tran <grimshady.z@gmail.com>
2016-05-06 16:46:38 +0700

    Dropdown list for gender choosing + style

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -25,7 +25,6 @@ class ProfilesController < ApplicationController
   # POST /profiles.json
   def create
     @profile = current_user.build_profile(profile_params)
-
     respond_to do |format|
       if @profile.save
         format.html { redirect_to user_path(current_user), notice: 'Profile was successfully created.' }
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,6 @@ class Profile < ActiveRecord::Base
   validates :avatar, attachment_presence: true
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  GENDERS =	['Male','Female']
+
 end
