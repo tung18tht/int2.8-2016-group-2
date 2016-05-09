daa4704972a4050177fbb50b541209c3eab6dd11
rosemarytran <ththao111@gmail.com>
2016-05-09 10:20:07 +0000

    Validate user_name, age, location

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,10 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  validates :user_name, presence: true, length: { minimum: 2 }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 18 }
+  validates :location, presence: true
+  #validates :intro, presence: true
+  #validates :interest, presence: true
   validates :avatar, attachment_presence: true
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
