a1f6c3422e5615bbaa51e6d9c62127151adb3d20
duy-tran <grimshady.z@gmail.com>
2016-05-14 01:51:04 +0700

    Sort users in find friends page based on number of common interest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
     def index
         @users = User.all
+        unless current_user.nil? || current_user.profile.nil?
+            @this_user_interests = current_user.profile.interest.split(";")
+            @common_interest = Hash.new(0)
+            @users.each do |user|
+                if user.id == current_user.id || user.profile.nil? || current_user.likes?(user)
+                    @users -= [user]
+                    next
+                end
+                @other_user_interests = user.profile.interest.split(";")
+                @common_interest[user.id] = (@this_user_interests & @other_user_interests).size
+            end
+            @users.sort_by! {|user| -@common_interest[user.id]}
+        end
     end
     
     def show
