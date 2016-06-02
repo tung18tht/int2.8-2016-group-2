77bdf563837f48b82e60aa90aec75c22300606fe
Duy <grimshady.z@gmail.com>
2016-06-02 10:16:33 +0700

    Clean uploaded files after test

--- features/support/upload_cleaner.rb
+++ features/support/upload_cleaner.rb
@@ -1,10 +1,10 @@
 # # Removes uploaded files when all scenarios for the current test process
 # # are finished. Ready for parallel_tests, too.
 
-# require 'fileutils'
+require 'fileutils'
 
-# at_exit do
-#   directory_name = "#{ Rails.env }#{ ENV['TEST_ENV_NUMBER'] }"
-#   uploads_path = Rails.root.join('public/system', directory_name)
-#   FileUtils.remove_dir(uploads_path) if uploads_path.directory?
-# end
+at_exit do
+  directory_name = "#{ Rails.env }#{ ENV['TEST_ENV_NUMBER'] }"
+  uploads_path = Rails.root.join('public/system', directory_name)
+  FileUtils.remove_dir(uploads_path) if uploads_path.directory?
+end
